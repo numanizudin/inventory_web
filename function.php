13bbc7ea67457c12842c1c86355585feff9c699f
numanizudin <muhammadnumanizudin@gmail.com>
2023-08-20 14:17:12 +0700

    Barang Keluar

--- function.php
+++ function.php
@@ -4,4 +4,46 @@ session_start();
 //Koneksi ke Database
 $conn = mysqli_connect("localhost", "root", "", "inventory-web");
 
+//Menambah barang baru
+if (isset($_POST['addnewbarang'])) {
+    $namabarang = $_POST['namabarang'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+
+    $addtotable = mysqli_query($conn, "INSERT INTO stock (nama_barang, deskripsi, stock) VALUES('$namabarang','$deskripsi', '$stock')");
+    if ($addtotable) {
+        header('location:index.php');
+    } else {
+        echo 'Gagal';
+        header('location:index.php');
+
+    }
+}
+;
+
+//Menambah barang masuk
+if (isset($_POST['barangmasuk'])) {
+    $barangnya = $_POST['barangnya'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $cekstockbarang = mysqli_query($conn, "SELECT * FROM stock WHERE id_barang='$barangnya'");
+    $ambildatanya = mysqli_fetch_array($cekstockbarang);
+
+    $stocksekarang = $ambildatanya['stock'];
+    $tambahkanstocksekarangdenganquantity = $stocksekarang + $qty;
+
+
+
+    $addtomasuk = mysqli_query($conn, "INSERT INTO masuk (id_barang, keterangan, qty) VALUES('$barangnya', '$penerima', '$qty')");
+    $updatestockmasuk = mysqli_query($conn, "UPDATE stock SET stock='$tambahkanstocksekarangdenganquantity' WHERE id_barang='$barangnya'");
+    if ($addtomasuk && $updatestockmasuk) {
+        header('location:masuk.php');
+    } else {
+        echo 'Gagal';
+        header('location:masuk.php');
+
+    }
+}
+
 ?>
